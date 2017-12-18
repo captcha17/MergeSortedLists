e33acc2231ecce02adf4e456c5cf5e99745a32d2
Dzmitry <Dzmitry.Rakushau@thomsonreuters.com>
2017-12-18 16:17:47 +0300

    Make refactoring

--- src/Runner.java
+++ src/Runner.java
@@ -1,98 +1,117 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Runner
 {
-    private static List<Integer> mergeSortedListsByStream(List<List<Integer>> lists)
-    {
-        return lists.stream()
-                .flatMap(Collection::stream)
-                .sorted()
-                .collect(Collectors.toList());
-    }
+	private static List<Integer> mergeSortedListsByStream(List<List<Integer>> lists)
+	{
+		return lists.stream()
+				.flatMap(integers -> integers.stream())
+				.sorted()
+				.collect(Collectors.toList());
+	}
 
-    private static List<Integer> mergeSortedListsByLoopsAndCollectionsSort(List<List<Integer>> lists)
-    {
-        int totalSize = 0;
-        for (List<Integer> list : lists) {
-            totalSize += list.size();
-        }
-        List<Integer> result = new ArrayList<>(totalSize);
+	private static List<Integer> mergeSortedListsByLoopsAndCollectionsSort(List<List<Integer>> lists)
+	{
+		int totalSize = 0;
+		for (List<Integer> list : lists) {
+			totalSize += list.size();
+		}
+		List<Integer> result = new ArrayList<>(totalSize);
 
-        for (List<Integer> list : lists) {
-            for (Integer i: list) {
-                result.add(i);
-            }
-        }
+		for (List<Integer> list : lists) {
+			for (Integer i: list) {
+				result.add(i);
+			}
+		}
 
-        Collections.sort(result);
-        return result;
-    }
+		Collections.sort(result);
+		return result;
+	}
 
-    private static List<Integer> mergeSortedListsByComparingAndRemoving(List<List<Integer>> lists) {
-        int totalSize = 0;
-        for (List<Integer> list : lists) {
-            totalSize += list.size();
-        }
+	private static List<Integer> mergeSortedListsByComparingAndRemoving(List<List<Integer>> lists) {
+		int totalSize = 0;
+		for (List<Integer> list : lists) {
+			totalSize += list.size();
+		}
 
-        List<Integer> result = new ArrayList<>(totalSize);
+		List<Integer> result = new ArrayList<>(totalSize);
 
-        List<Integer> lowest;
+		List<Integer> lowest;
 
-        while (result.size() < totalSize) {
-            lowest = null;
+		while (result.size() < totalSize) {
+			lowest = null;
 
-            for (List<Integer> list : lists) {
-                if (!list.isEmpty()) {
-                    if (lowest == null) {
-                        lowest = list;
-                    } else if (list.get(0).compareTo(lowest.get(0)) <= 0) {
-                        lowest = list;
-                    }
-                }
-            }
+			for (List<Integer> list : lists) {
+				if (!list.isEmpty()) {
+					if (lowest == null) {
+						lowest = list;
+					} else if (list.get(0).compareTo(lowest.get(0)) <= 0) {
+						lowest = list;
+					}
+				}
+			}
 
-            result.add(lowest.get(0));
-            lowest.remove(0);
-        }
+			result.add(lowest.get(0));
+			lowest.remove(0);
+		}
 
-        return result;
-    }
+		return result;
+	}
+	
+	private static List<List<Integer>> getCommonList()
+	{
+		List<List<Integer>> commonList = new ArrayList<>();
+		for (int i = 1; i < 10; i++)
+		{
+			List<Integer> list = new ArrayList<>();
+			for (int j = 1; j < 1000; j = j+i)
+			{
+				list.add(j);
+			}
+			
+			commonList.add(list);
+		}
+		
+		return commonList;
+	}
 
-    public static void main(String[] args)
-    {
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        List<Integer> list3 = new ArrayList<>();
-        for (int i = 1; i < 50; i = i + 2) {
-            list1.add(i);
-        }
-        for (int i = 1; i < 50; i = i + 3) {
-            list2.add(i);
-        }
-        for (int i = 1; i < 50; i = i + 4) {
-            list3.add(i);
-        }
+	public static void main(String[] args)
+	{
+		List<List<Integer>> commonList;
+		long begin;
+		List<Integer> integers;
 
-        List<List<Integer>> commonList = Arrays.asList(list1, list2, list3);
+		commonList = getCommonList();
+		begin = System.nanoTime();
+		integers = mergeSortedListsByStream(commonList);
+		System.out.println("Streams initial.Execution time = " + (System.nanoTime() - begin)/1000);
+		integers.forEach(i -> System.out.print(i + " "));
 
-        long begin = System.nanoTime();
-        List<Integer> integers = mergeSortedListsByStream(commonList);
-        System.out.println("Streams.Execution time = " + (System.nanoTime() - begin)/1000);
-        integers.forEach(i -> System.out.print(i + " "));
+		System.out.println();
 
-        System.out.println();
+		commonList = getCommonList();
+		begin = System.nanoTime();
+		integers = mergeSortedListsByStream(commonList);
+		System.out.println("Streams warm.Execution time = " + (System.nanoTime() - begin)/1000);
+		integers.forEach(i -> System.out.print(i + " "));
 
-        begin = System.nanoTime();
-        List<Integer> integers2 = mergeSortedListsByLoopsAndCollectionsSort(commonList);
-        System.out.println("CollectionsSort.Execution time = " + (System.nanoTime() - begin)/1000);
-        integers2.forEach(i -> System.out.print(i + " "));
+		System.out.println();
 
-        System.out.println();
+		commonList = getCommonList();
+		begin = System.nanoTime();
+		integers = mergeSortedListsByLoopsAndCollectionsSort(commonList);
+		System.out.println("CollectionsSort.Execution time = " + (System.nanoTime() - begin)/1000);
+		integers.forEach(i -> System.out.print(i + " "));
 
-        begin = System.nanoTime();
-        List<Integer> integers3 = mergeSortedListsByComparingAndRemoving(commonList);
-        System.out.println("ComparingAndRemoving.Execution time = " + (System.nanoTime() - begin)/1000);
-        integers3.forEach(i -> System.out.print(i + " "));
-    }
+		System.out.println();
+
+		commonList = getCommonList();
+		begin = System.nanoTime();
+		integers = mergeSortedListsByComparingAndRemoving(commonList);
+		System.out.println("ComparingAndRemoving.Execution time = " + (System.nanoTime() - begin)/1000);
+		integers.forEach(i -> System.out.print(i + " "));
+	}
 }
